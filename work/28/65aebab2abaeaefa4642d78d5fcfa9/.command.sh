82bee0f7ae51778dc94080e1cf60f82c2b5d7106
Vinitha Nadar <vinitanadar9503@gmail.com>
2026-03-16 17:41:12 +0000

    Fixed the trichammatic output dir

---
+++ work/28/65aebab2abaeaefa4642d78d5fcfa9/.command.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -ue
+trimmomatic PE -phred33 A_33_FDSW202661760-1r_HTNK5DSXY_L3_sub_1.fq.gz A_33_FDSW202661760-1r_HTNK5DSXY_L3_sub_2.fq.gz A_33_FDSW202661760-1r_HTNK5DSXY_L3_sub_1.paired.fq.gz A_33_FDSW202661760-1r_HTNK5DSXY_L3_sub_1.unpaired.fq.gz A_33_FDSW202661760-1r_HTNK5DSXY_L3_sub_2.paired.fq.gz A_33_FDSW202661760-1r_HTNK5DSXY_L3_sub_2.unpaired.fq.gz ILLUMINACLIP:/Users/vinithanadar/miniconda/envs/nf_course/share/trimmomatic-0.40-0/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
